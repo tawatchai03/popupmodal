e6aec01b62d110fb12f4059de9875cb73b9dc5ff
mai_tawat <m.tawatchai03@gmail.com>
2022-08-15 16:27:17 +0700

    upp

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 window.addEventListener("load", function() {
-    setTimeout(
-        function open(event) {
-            document.querySelector(".popup").style.display = "block";
-        },
-        1000
-    )
+    var currentTime = new Date();
+    var startTime = new Date();
+    var endTime = new Date();
+
+    startTime.setHours(11);
+    startTime.setMinutes(0);
+    startTime.setSeconds(0);
+    endTime.setHours(16);
+    endTime.setMinutes(26);
+    endTime.setSeconds(0);
+    currentTime.getHours();
+    currentTime.getMinutes();
+    currentTime.getSeconds();
+
+    if (currentTime >= startTime) {
+        console.log("ok");
+        document.querySelector(".popup").style.display = "block";
+    }
+    if (currentTime >= endTime) {
+
+        console.log("no");
+        document.querySelector(".popup").style.display = "none";
+    }
 });
 
 
