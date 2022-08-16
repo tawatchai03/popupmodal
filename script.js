577fcbd52bd6dcf6b4ca7973f2f5aeb002994a5d
mai_tawat <m.tawatchai03@gmail.com>
2022-08-16 07:03:58 +0700

    up

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ window.addEventListener("load", function() {
     var startTime = new Date();
     var endTime = new Date();
     console.log(zoneTime);
-
+    //เริ่มทำงาน
     startTime.setDate(15);
     startTime.setMonth(8);
     startTime.setYear(22);
-    startTime.setHours(08);
+    startTime.setHours(10);
     startTime.setMinutes(0);
     startTime.setSeconds(0);
-
-    endTime.setDate(15);
+    //จบการทำงาน
+    endTime.setDate(16);
     endTime.setMonth(8);
     endTime.setYear(22);
-    endTime.setHours(18);
-    endTime.setMinutes(8);
+    endTime.setHours(5);
+    endTime.setMinutes(0);
     endTime.setSeconds(0);
 
-    currentTime.setDate(15);
+    currentTime.setDate(16);
     currentTime.setMonth(8);
     currentTime.setYear(22);
     currentTime.getHours();
@@ -27,19 +27,19 @@ window.addEventListener("load", function() {
     currentTime.getSeconds();
 
     if (currentTime >= startTime) {
-        document.querySelector(".popup").style.display = "block";
+        document.querySelector(".popup-modal").style.display = "block";
     }
     if (currentTime >= endTime) {
-        document.querySelector(".popup").style.display = "none";
+        document.querySelector(".popup-modal").style.display = "none";
     }
 });
 document.querySelector("#close").addEventListener("click", function() {
-    document.querySelector(".popup").style.display = "none";
+    document.querySelector(".popup-modal").style.display = "none";
 });
 
 
 document.body.addEventListener("click", function() {
-    document.querySelector(".popup").style.display = "none";
+    document.querySelector(".popup-modal").style.display = "none";
 });
 
 // document.body.addEventListener('click', () => {
